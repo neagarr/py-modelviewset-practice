6d947abe87f798a9e8fb9a0b96ec71b7003d4d2e
Mykola Kokoiev <veokoknik@gmail.com>
2025-03-18 23:16:59 +0100

    add class Meta

--- author/models.py
+++ author/models.py
@@ -7,3 +7,10 @@ class Author(models.Model):
     pseudonym = models.CharField(max_length=64, null=True, blank=True)
     age = models.IntegerField()
     retired = models.BooleanField()
+
+    class Meta:
+        ordering = ("first_name", "last_name",)
+        verbose_name_plural = "authors"
+
+    def __str__(self):
+        return self.first_name
