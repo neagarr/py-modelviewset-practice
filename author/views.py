e186bfcccf3a72e73a9d47a3c2a64ff6014a9180
Mykola Kokoiev <veokoknik@gmail.com>
2025-03-19 00:54:32 +0100

    fix

--- author/views.py
+++ author/views.py
@@ -1,8 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from author.models import Author
 from author.serializers import AuthorSerializer
 
 
-class AuthorViewSet(viewsets.ModelViewSet):
+class AuthorViewSet(
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
